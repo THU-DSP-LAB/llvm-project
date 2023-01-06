6959b660221fe23fd4ac12cdffc94e308d72a559
Aries <aries.wu@terapines.com>
2023-01-06 09:32:07 +0800

    Add CSRs and kernel metadata buffer offset constant definition ventus.h

---
+++ compiler-rt/lib/ventus/ventus.h
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2023 Terapines Technology (Wuhan) Co., Ltd
+ *
+ * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See https://llvm.org/LICENSE.txt for license information.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+/**
+ * This file defines the hardware CSR and kernel metadata buffer related const.
+ *
+ * kernel metadata buffer layout:
+ * +-------4---------+----------4----------+-----4----+-------4-------+-------4-------
+ * | kernel func ptr | kernel arg base ptr | work_dim | global_size_x | global_size_y
+ * +-------4-------+------4-------+------4-------+------4-------+-------4---------
+ * | global_size_z | local_size_x | local_size_y | local_size_z | global_offset_x
+ * +--------4--------+--------4--------+----
+ * | global_offset_y | global_offset_z | ...
+ */
+
+#ifndef __VENTUS_H__
+#define __VENTUS_H__
+
+// CSRs
+#define CSR_TID   0x800
+#define CSR_NUMW  0x801
+#define CSR_NUMT  0x802
+#define CSR_KNL   0x803    // Kernel metadata buffer base address
+#define CSR_WID   0x805
+#define CSR_LDS   0x806
+#define CSR_GDS   0x807
+#define CSR_GID_X 0x808    // group_id_x
+#define CSR_GID_Y 0x809
+#define CSR_GID_Z 0x80a
+
+// Kernel metadata buffer offsets
+#define KNL_ENTRY 0
+#define KNL_ARG_BASE 4
+#define KNL_WORK_DIM 8
+#define KNL_GL_SIZE_X 12
+#define KNL_GL_SIZE_Y 16
+#define KNL_GL_SIZE_Z 20
+#define KNL_LC_SIZE_X 24
+#define KNL_LC_SIZE_Y 28
+#define KNL_LC_SIZE_Z 32
+#define KNL_GL_OFFSET_X 36
+#define KNL_GL_OFFSET_Y 40
+#define KNL_GL_OFFSET_Z 44
+
+#endif // __VENTUS_H__
