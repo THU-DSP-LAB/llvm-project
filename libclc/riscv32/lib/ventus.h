851461a2f0c0c5378013413dd1082366ae21b563
zhoujingya <jing.zhou@terapines.com>
2023-03-03 13:04:48 +0800

    Update ventus.h for adding print buffer runtime information

--- libclc/riscv32/lib/ventus.h
+++ libclc/riscv32/lib/ventus.h
@@ -13,24 +13,25 @@
  * | kernel func ptr | kernel arg base ptr | work_dim | global_size_x | global_size_y
  * +-------4-------+------4-------+------4-------+------4-------+-------4---------
  * | global_size_z | local_size_x | local_size_y | local_size_z | global_offset_x
- * +--------4--------+--------4--------+----
- * | global_offset_y | global_offset_z | ...
+ * +--------4--------+--------4--------+------4-----+------4-----+
+ * | global_offset_y | global_offset_z | print_addr | print_size |
  */
 
 #ifndef __VENTUS_H__
 #define __VENTUS_H__
 
 // CSRs
-#define CSR_TID   0x800
-#define CSR_NUMW  0x801
-#define CSR_NUMT  0x802
+#define CSR_TID   0x800    // smallest thread id in a warp
+#define CSR_NUMW  0x801    // warp numbers in a workgroup
+#define CSR_NUMT  0x802    // thread numbers in a warp
 #define CSR_KNL   0x803    // Kernel metadata buffer base address
-#define CSR_WID   0x805
-#define CSR_LDS   0x806
-#define CSR_GDS   0x807
+#define CSR_WID   0x805    // warp id in a workgroup
+#define CSR_LDS   0x806    // baseaddr for local memory allocated by workgroup
+#define CSR_PDS   0x807    // baseaddr for private memory allocated by workgroup
 #define CSR_GID_X 0x808    // group_id_x
-#define CSR_GID_Y 0x809
-#define CSR_GID_Z 0x80a
+#define CSR_GID_Y 0x809    // group_id_y
+#define CSR_GID_Z 0x80a    // group_id_z
+#define CSR_PRINT 0x80b    // for print buffer
 
 // Kernel metadata buffer offsets
 #define KNL_ENTRY 0
@@ -45,5 +46,7 @@
 #define KNL_GL_OFFSET_X 36
 #define KNL_GL_OFFSET_Y 40
 #define KNL_GL_OFFSET_Z 44
+#define KNL_PRINT_ADDR 48
+#define KNL_PRINT_SIZE 52
 
 #endif // __VENTUS_H__
