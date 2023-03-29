de8b6d5479771dd93c4f3099fc533ad97034ebf8
zhoujingya <jing.zhou@terapines.com>
2023-03-29 21:42:44 +0800

    Add vsetvli instruction in crt0 && remove barrier headers in libclc
    Signed-off-by: zhoujingya <jing.zhou@terapines.com>

--- libclc/generic/include/clc/clc.h
+++ libclc/generic/include/clc/clc.h
@@ -204,7 +204,6 @@
 
 /* 6.11.8 Synchronization Functions */
 #include <clc/synchronization/cl_mem_fence_flags.h>
-#include <clc/synchronization/barrier.h>
 
 /* 6.11.9 Explicit Memory Fence Functions */
 #include <clc/explicit_fence/explicit_memory_fence.h>
--- libclc/generic/include/clc/synchronization/barrier.h
+++
@@ -1,3 +0,0 @@
-_CLC_DECL _CLC_OVERLOAD void barrier(cl_mem_fence_flags flags);
-_CLC_DECL _CLC_OVERLOAD void work_group_barrier(cl_mem_fence_flags, memory_scope);
-_CLC_DECL _CLC_OVERLOAD void work_group_barrier(cl_mem_fence_flags);
