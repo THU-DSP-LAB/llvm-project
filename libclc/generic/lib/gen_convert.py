e8492c3117e630414dff97af1f47664724be5ff8
zhoujing <jing.zhou@terapines.com>
2023-07-06 21:47:55 +0800

    [VENTUS][RISCV][fix] fix undefined symbol erros in libclc library

--- libclc/generic/lib/gen_convert.py
+++ libclc/generic/lib/gen_convert.py
@@ -26,19 +26,17 @@
 #
 # convert_<destTypen><_sat><_roundingMode>(<sourceTypen>)
 
-types = ['char', 'uchar', 'short', 'ushort', 'int', 'uint', 'long', 'ulong', 'float', 'double']
-int_types = ['char', 'uchar', 'short', 'ushort', 'int', 'uint', 'long', 'ulong']
-unsigned_types = ['uchar', 'ushort', 'uint', 'ulong']
-float_types = ['float', 'double']
-int64_types = ['long', 'ulong']
-float64_types = ['double']
+types = ['char', 'uchar', 'short', 'ushort', 'int', 'uint', 'float']
+int_types = ['char', 'uchar', 'short', 'ushort', 'int', 'uint']
+unsigned_types = ['uchar', 'ushort', 'uint']
+float_types = ['float']
 vector_sizes = ['', '2', '3', '4', '8', '16']
 half_sizes = [('2',''), ('4','2'), ('8','4'), ('16','8')]
 
 saturation = ['','_sat']
 rounding_modes = ['_rtz','_rte','_rtp','_rtn']
 float_prefix = {'float':'FLT_', 'double':'DBL_'}
-float_suffix = {'float':'f', 'double':''}
+float_suffix = {'float':'f'}
 
 bool_type = {'char'  : 'char',
              'uchar' : 'char',
@@ -46,25 +44,19 @@ bool_type = {'char'  : 'char',
              'ushort': 'short',
              'int'   : 'int',
              'uint'  : 'int',
-             'long'  : 'long',
-             'ulong' : 'long',
-             'float'  : 'int',
-             'double' : 'long'}
+             'float'  : 'int'}
 
 unsigned_type = {'char'  : 'uchar',
                  'uchar' : 'uchar',
                  'short' : 'ushort',
                  'ushort': 'ushort',
                  'int'   : 'uint',
-                 'uint'  : 'uint',
-                 'long'  : 'ulong',
-                 'ulong' : 'ulong'}
+                 'uint'  : 'uint'}
 
 sizeof_type = {'char'  : 1, 'uchar'  : 1,
                'short' : 2, 'ushort' : 2,
                'int'   : 4, 'uint'   : 4,
-               'long'  : 8, 'ulong'  : 8,
-               'float' : 4, 'double' : 8}
+               'float' : 4}
 
 limit_max = {'char'  : 'CHAR_MAX',
              'uchar' : 'UCHAR_MAX',
@@ -87,14 +79,14 @@ limit_min = {'char'  : 'CHAR_MIN',
 def conditional_guard(src, dst):
   int64_count = 0
   float64_count = 0
-  if src in int64_types:
-    int64_count = int64_count +1
-  elif src in float64_types:
-    float64_count = float64_count + 1
-  if dst in int64_types:
-    int64_count = int64_count +1
-  elif dst in float64_types:
-    float64_count = float64_count + 1
+  # if src in int64_types:
+  #   int64_count = int64_count +1
+  # elif src in float64_types:
+  #   float64_count = float64_count + 1
+  # if dst in int64_types:
+  #   int64_count = int64_count +1
+  # elif dst in float64_types:
+  #   float64_count = float64_count + 1
   if float64_count > 0:
     #In embedded profile, if cl_khr_fp64 is supported cles_khr_int64 has to be
     print("#ifdef cl_khr_fp64")
