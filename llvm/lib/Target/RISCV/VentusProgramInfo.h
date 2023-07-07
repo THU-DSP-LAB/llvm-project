7da8486eb3ce08482497806005b8a052342cb354
zhoujing <jing.zhou@terapines.com>
2023-07-07 15:42:25 +0800

    [VENTUS][RISCV][feat] Add data structure interface for collecting ventus program information

---
+++ llvm/lib/Target/RISCV/VentusProgramInfo.h
@@ -0,0 +1,33 @@
+//===------------------------ VentusProgramInfo.h - -----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Define a strcut to collect the resource usage information of Ventus
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef VENTUS_PROGRAM_INFO_H
+#define VENTUS_PROGRAM_INFO_H
+
+#include <cstdint>
+
+namespace llvm {
+
+  struct VentusProgramInfo {
+
+    uint32_t VGPRUsage = 265; //The number of VGPRS which has been used
+    uint32_t SGPRUsage = 265; //The number of SGPRS which has been used
+    uint32_t LDSMemory = 0; //The number of VGPRS which has been used
+    uint32_t PDSMemory = 1 << 10; //The number of VGPRS which has been used
+
+    VentusProgramInfo() = default;
+
+  };
+
+} // namespace llvm
+
+#endif // VENTUS_PROGRAM_INFO_H
